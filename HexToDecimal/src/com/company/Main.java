1ddef567506301d3ba7ca11dba55f86e0fa5cf26
yangyangma <yangyangma2016@gmail.com>
2019-05-16 14:32:01 -0400

    no message

---
+++ HexToDecimal/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Please input a hex number:");
+        Scanner sc = new Scanner(System.in);
+        string s = sc.nextLine();
+
+
+
+    }
+}
